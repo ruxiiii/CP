41f2271317925fc20b13a09829965f93e1a29f08
ruxiiii <71703227+ruxiiii@users.noreply.github.com>
2023-08-19 18:36:59 +0530

    create Index_of_first_occurance.py

---
+++ Index_of_first_occurance.py
@@ -0,0 +1,21 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        '''first length of needle
+        then the first occurance of needle in haystack
+        if length needle<len hay return -1
+        '''
+
+        if not needle:
+            return (-1)
+
+      
+        
+        len_needle = len(needle)
+        difference_length = len(haystack) - len(needle)
+
+        for i in range(difference_length + 1):
+            if haystack[i:(len(needle) + i)] == needle:
+                return i
+            
+            return (-1)
+
